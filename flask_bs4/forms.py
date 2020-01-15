6f976f107bd56725ac3a2e1cf23b20fef09e8c54
Horia Filimonescu <horia.filimonescu+github@gmail.com>
2020-01-15 20:58:57 +0100

    Fix: `forms.render_form` creates correct fields

--- flask_bs4/forms.py
+++ flask_bs4/forms.py
@@ -1,144 +1,174 @@
-from dominate import tags
-from dominate.util import raw
 from markupsafe import Markup
-from visitor import Visitor
 
+from wtforms.widgets.core import html_params
+
+def _wrap_form(form,
+               action='',
+               id=None,
+               method='post',
+               enctype=None,
+               **kwargs):
+    form_content = f'<form action="{action}" method="{method}" id="{id}" class="form" role="form" enctype="{enctype[0]}">\n'
+    form_content += f'{form}\n</form>'
+
+    return form_content
+
+def _wrap_field(field, **kwargs):
+    content = ''
+    form_type = kwargs.get('form_type', 'basic')
+    cols = kwargs.get('horizontal_columns', ('lg', 0, 12))
+
+    if form_type == 'horizontal':
+        col1 = 'col-{}-{}'.format(cols[0], cols[1])
+        col2 = 'col-{}-{}'.format(cols[0], cols[2])
+        off1 = 'offset-{}-{}'.format(cols[0], cols[1])
+
+        content += '<div class="form-group row">\n'
+        content += f'\t<div class="{col1}">{field.label}</div>\n'
+        content += f'\t<div class="{col2}">{field(class_="form-control")}</div>\n'
+        content += _add_description(field, **kwargs)
+        content += '</div>\n'
+    else:
+        content += f'<div class="form-group">{field.label} {field(class_="form-control")}'
+        content += _add_description(field, **kwargs)
+        content += '</div>\n'
+    return content
+
+def _add_description(field, **kwargs):
+    content = ''
+
+    form_type = kwargs.get('form_type', 'basic')
+    cols = kwargs.get('horizontal_columns', ('lg', 0, 12))
+
+    col1 = f'col-{cols[0]}-{cols[1]}'
+    col2 = f'col-{cols[0]}-{cols[2]}'
+    off1 = f'offset-{cols[0]}-{cols[1]}'
+
+    content += f'<small id="{ field.name }Help" class="form-text text-muted { off1 } { col2 }">'
+    content += f'{ field.description }</small>'
+
+    return content
+
+def _wrap_boolean(field, **kwargs):
+    content = ''
+    form_type = kwargs.get('form_type', 'basic')
+    horizontal_columns = kwargs.get('horizontal_columns', ('lg', 0, 12))
+
+    col1 = 'col-{}-{}'.format(horizontal_columns[0], horizontal_columns[1])
+    col2 = 'col-{}-{}'.format(horizontal_columns[0], horizontal_columns[2])
+    off1 = 'offset-{}-{}'.format(horizontal_columns[0], horizontal_columns[1])
+
+    content += f'<div class="form-group { "row" if form_type == "horizontal" else "" }">\n'
+    content += f'\t<div class="{ off1 if form_type == "horizontal" else "" } { col2 if form_type == "horizontal" else "" }">'
+    content += f'<div class="form-check">'
+    content += f'{field(class_="form-check-input")}'
+    content += f'{field.label(class_="form-check-label")}'
+    content += f'</div>'
+    content += f'</div>\n'
+    content += _add_description(field, **kwargs)
+    content += '</div>\n'
+
+    return content
+
+def _wrap_radio(field, **kwargs):
+    content = ''
+    form_type = kwargs.get('form_type', 'basic')
+    horizontal_columns = kwargs.get('horizontal_columns', ('lg', 0, 12))
+
+    col1 = 'col-{}-{}'.format(horizontal_columns[0], horizontal_columns[1])
+    col2 = 'col-{}-{}'.format(horizontal_columns[0], horizontal_columns[2])
+    off1 = 'offset-{}-{}'.format(horizontal_columns[0], horizontal_columns[1])
+
+    if form_type == 'horizontal':
+        content += '<div class="form-group row">\n'
+        content += f'\t<legend class="col-form-label {col1} pt-0">{field.label}</legend>\n'
+        content += f'\t<div class="form-check {col2}">'
+        for key, value in field.choices:
+            content += f'<div class="form-check {col2}">\n'
+            content += f'<input class="form-check-input" type="radio" name="{field.name}" id="{key}" value="{key}" {"checked" if key == field.default else ""}>\n'
+            content += f'<label class="form-check-label" for="{key}">{value}</label>'
+            content += f'</div>\n'
+        content += '</div>\n'
+        content += _add_description(field, **kwargs)
+        content += '</div>\n'
+    else:
+        content += '<div class="form-group">\n'
+        content += f'\t<legend class="col-form-label pt-0">{field.label}</legend>\n'
+        content += f'\t<div class="form-check">'
+        for key, value in field.choices:
+            content += f'<div class="form-check">\n'
+            content += f'<input class="form-check-input" type="radio" name="{field.name}" id="{key}" value="{key}" {"checked" if key == field.default else ""}>\n'
+            content += f'<label class="form-check-label" for="{key}">{value}</label>'
+            content += f'</div>\n'
+        content += '</div>\n'
+        content += _add_description(field, **kwargs)
+        content += '</div>\n'
+
+    return content
+
+def _wrap_file(field, **kwargs):
+    content = ''
+    ft = kwargs.get('form_type', 'basic')
+    cols = kwargs.get('horizontal_columns', ('lg', 0, 12))
+
+    col1 = 'col-{}-{}'.format(cols[0], cols[1])
+    col2 = 'col-{}-{}'.format(cols[0], cols[2])
+    off1 = 'offset-{}-{}'.format(cols[0], cols[1])
+
+    content += f'<div class="form-group { "row" if ft == "horizontal" else "" }">\n'
+    content += f'\t<div class="{ col1 if ft == "horizontal" else "" }"></div>\n'
+    content += f'\t<div class="{ col2 if ft == "horizontal" else "" }">\n'
+    content += f'\t<div class="custom-file">\n'
+    content += field(class_='custom-file-input')
+    content += field.label(class_='custom-file-label')
+    content += f'\n\t</div>\n'
+    content += f'\n\t</div>\n'
+    content += _add_description(field, **kwargs)
+    content += f'</div>\n'
+
+    return content
+
+def _wrap_submit(field, **kwargs):
+    content = ''
+    form_type = kwargs.get('form_type', 'basic')
+    horizontal_columns = kwargs.get('horizontal_columns', ('lg', 0, 12))
+    button_map = kwargs.get('button_map', {'submit': 'primary'})
+
+    col1 = 'col-{}-{}'.format(horizontal_columns[0], horizontal_columns[1])
+    col2 = 'col-{}-{}'.format(horizontal_columns[0], horizontal_columns[2])
+    off1 = 'offset-{}-{}'.format(horizontal_columns[0], horizontal_columns[1])
+
+    content += f'<div class="form-group { "row" if form_type == "horizontal" else "" }">\n'
+    content += f'\t<div class="{ col1 if form_type == "horizontal" else "" }"></div>\n'
+    content += f'\t<div class="{ col2 if form_type == "horizontal" else "" }">\n'
+    content += field(class_=f'btn btn-{ button_map.get(field.name) }')
+    content += f'\n\t</div>\n'
+    content += _add_description(field, **kwargs)
+    content += f'</div>\n'
+
+    return content
+
+def _wrap_csrf(field):
+    return field()
 
 def render_form(form, **kwargs):
-    r = WTFormsRenderer(**kwargs)
+    form_fields = ''
+    _enctype = []
 
-    return Markup(r.visit(form))
+    for field in form:
+        if field.type == 'BooleanField':
+            form_fields += _wrap_boolean(field, **kwargs)
+        elif field.type == 'RadioField':
+            form_fields += _wrap_radio(field, **kwargs)
+        elif field.type == 'FileField':
+            if not _enctype:
+                _enctype.append('multipart/form-data')
+            form_fields += _wrap_file(field, **kwargs)
+        elif field.type == 'SubmitField':
+            form_fields += _wrap_submit(field, **kwargs)
+        elif field.type == 'CSRFTokenField':
+            form_fields += _wrap_csrf(field)
+        else:
+            form_fields += _wrap_field(field, **kwargs)
 
-
-class WTFormsRenderer(Visitor):
-    def __init__(self,
-                 action='',
-                 id=None,
-                 method='post',
-                 extra_classes=[],
-                 role='form',
-                 form_type="basic",
-                 horizontal_columns=('lg', 3, 9),
-                 button_map={},
-                 enctype=None):
-        self.action = action
-        self.form_id = id
-        self.method = method
-        self.extra_classes = extra_classes
-        self.role = role
-        self.form_type = form_type
-        self.horizontal_columns = horizontal_columns
-        self.button_map = button_map
-        self.enctype = enctype
-
-        if self.form_type in ['horizontal', 'inline']:
-            self.extra_classes.append('form-{}'.format(self.form_type))
-            self.col1 = 'col-{}-{} '.format(self.horizontal_columns[0],
-                                            self.horizontal_columns[1])
-            self.col2 = 'col-{}-{} '.format(self.horizontal_columns[0],
-                                            self.horizontal_columns[2])
-
-    def _wrapper(self, node, classes=['form-group']):
-        if self.form_type == 'horizontal':
-            classes.append('row')
-
-        return tags.div(_class=' '.join(classes))
-
-    def _wrapped_field(self, node, type='text', classes=['form-control'], **kwargs):
-        exceptions = ['BooleanField',
-                      'FileField',
-                      'RadioField',
-                      'SubmitField']
-
-        if self.form_type == 'horizontal':
-            classes.append('row')
-
-        if node.flags.hidden:
-            return raw(node())
-
-        if node.type in exceptions:
-            return self.visit(node)
-
-        if node.errors:
-            classes = ['form-control', 'is-invalid']
-
-        wrap = self._wrapper(node)
-        wrap.add(Markup(node.label))
-        wrap.add(Markup(node(class_=' '.join(classes))))
-
-        for error in node.errors:
-            wrap.add(tags.div(error, _class='invalid-feedback'))
-
-        if node.description:
-            wrap.add(tags.small(node.description, _class='form-text text-muted'))
-
-        return wrap
-
-    def visit_Form(self, node):
-        form = tags.form()
-
-        if self.extra_classes:
-            form['class'] = ' '.join(self.extra_classes)
-
-        if self.action:
-            form['action'] = self.action
-
-        if self.form_id:
-            form['id'] = self.form_id
-
-        if self.method:
-            form['method'] = self.method
-
-        self._real_enctype = self.enctype
-
-        for field in node:
-            form.add(self._wrapped_field(field))
-
-        if self._real_enctype:
-            form['enctype'] = self._real_enctype
-
-        return form
-
-    def visit_BooleanField(self, node):
-        wrap = self._wrapper(node, classes=['form-check'])
-
-        wrap.add(tags.input(type='checkbox', cls='form-check-input', id=node.id))
-        wrap.add(tags.label(node.label.text, _for=node.id, cls='form-check-label'))
-
-        if node.description:
-            wrap.add(tags.small(node.description, _class='form-text text-muted'))
-
-        return wrap
-
-    def visit_FileField(self, node):
-        wrap = self._wrapper(node)
-
-        cst = wrap.add(tags.div(cls='custom-file'))
-        cst.add(Markup(node(class_='custom-file-input')))
-        cst.add(Markup(node.label(class_='custom-file-label')))
-
-        if node.description:
-            wrap.add(tags.small(node.description, _class='form-text text-muted'))
-
-        return wrap
-
-    def visit_RadioField(self, node):
-        wrap = self._wrapper(node, classes=[])
-
-        for subfield in node:
-            subwrap = wrap.add(self._wrapper(node, classes=['form-check']))
-            subwrap.add(Markup(subfield(class_='form-check-input')))
-            subwrap.add(Markup(subfield.label(class_='form-check-label')))
-
-        return wrap
-
-    def visit_SubmitField(self, node):
-        try:
-            face = 'btn btn-' + self.button_map[node.name]
-        except KeyError:
-            face = 'btn btn-default'
-
-        button = tags.button(node.label.text,
-                             _class=face,
-                             type='submit')
-        return button
+    return Markup(_wrap_form(form_fields, enctype=_enctype, **kwargs))
