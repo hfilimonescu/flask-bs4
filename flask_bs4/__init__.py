204b04fb5205a7c05037f3271ef0b441bed3c181
Horia Filimonescu <horia.filimonescu+github@gmail.com>
2020-04-29 23:05:55 +0200

    * Bump version number to 4.4.1.8

--- flask_bs4/__init__.py
+++ flask_bs4/__init__.py
@@ -20,7 +20,7 @@ else:
 from .forms import render_field, render_form
 from .utils import flash_messages
 
-__version__ = '4.4.1.8.dev'
+__version__ = '4.4.1.8'
 BOOTSTRAP_VERSION = re.sub(r'^(\d+\.\d+\.\d+).*', r'\1', __version__)
 JQUERY_VERSION = '3.4.1'
 POPPER_VERSION = '1.16.0'
