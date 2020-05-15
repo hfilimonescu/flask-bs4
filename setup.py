7b6f02e1be273338aed7178da4bdff9f2db9d70c
Horia Filimonescu <horia.filimonescu+github@gmail.com>
2020-05-15 02:07:53 +0200

    Start development for 4.4.1.10.dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='Flask-BS4',
-    version='4.4.1.9',
+    version='4.4.1.10.dev',
     url='https://github.com/hfilimonescu/flask-bs4',
     license='MIT',
     author='Horia Filimonescu',
