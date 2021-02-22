aee25bda64944fa6edd5120147266fd59696184d
Horia Filimonescu <horia.filimonescu+github@gmail.com>
2021-02-22 13:33:56 +0100

    Utils: Floating toasts

--- flask_bs4/utils.py
+++ flask_bs4/utils.py
@@ -9,9 +9,21 @@ category = {
     'danger': 'bg-danger text-white',
     'warning': 'bg-warning text-black',
     'info': 'bg-info text-white',
-    'light': 'bg-light text-dark',
+    'light': 'bg-light text-black',
     'dark': 'bg-dark text-white',
-    'white': 'bg-white text-dark',
+    'white': 'bg-white text-black',
+}
+
+button = {
+    'primary': 'btn-close btn-close-white ms-auto me-2',
+    'secondary': 'btn-close btn-close-white ms-auto me-2',
+    'success': 'btn-close btn-close-white ms-auto me-2',
+    'danger': 'btn-close btn-close-white ms-auto me-2',
+    'warning': 'btn-close ms-auto me-2',
+    'info': 'btn-close ms-auto me-2',
+    'light': 'btn-close ms-auto me-2',
+    'dark': 'btn-close btn-close-white ms-auto me-2',
+    'white': 'btn-close ms-auto me-2',
 }
 
 
@@ -23,19 +35,13 @@ def flash_messages(messages=None, container=False, dismiss=True, autohide=True):
         _dismiss.append('alert-autohide')
 
     if current_app.config['BOOTSTRAP_USE_TOASTS']:
-        return flash_toasts(
-            messages=messages,
-            stacked=container,
-            autohide=autohide,
-        )
-    else:
-        return flash_alerts(
-            messages=messages,
-            container=container,
-            dismiss=' '.join(_dismiss)
-        )
+        return flash_toasts(messages=messages)
 
-    return ''
+    return flash_alerts(
+        messages=messages,
+        container=container,
+        dismiss=' '.join(_dismiss)
+    )
 
 
 def flash_alerts(messages=None, container=False, dismiss=True, autohide=False):
@@ -73,14 +79,8 @@ def flash_alerts(messages=None, container=False, dismiss=True, autohide=False):
     return wrap
 
 
-def flash_toasts(messages=None, stacked=False, autohide=False):
-    if stacked:
-        wrap = tags.div(
-            cls='toast-container')
-        row = wrap.add(tags.div(cls='row'))
-        col = row.add(tags.div(cls='col-md-12'))
-    else:
-        wrap = col = tags.div()
+def flash_toasts(messages=None):
+    wrap = tags.div()
 
     for message in messages:
         try:
@@ -92,14 +92,19 @@ def flash_toasts(messages=None, stacked=False, autohide=False):
         if cat == 'message':
             cat = 'dark'
 
-        with col:
+        with wrap:
             with tags.div():
-                tags.attr(cls=f'toast m-2 {category[cat]}')
+                tags.attr(
+                    cls=f'toast d-flex m-2 {category[cat]}')
                 tags.attr(role='alert')
-                with tags.div(cls=f'toast-header {category[cat]}'):
-                    tags.strong('Toast Message', cls='me-auto')
-                    tags.button(cls='btn-close', data_bs_dismiss='toast')
+                tags.attr(
+                    data_bs_delay=current_app.config.get(
+                        'BOOTSTRAP_TOAST_DELAY', 5000))
+                if not current_app.config['BOOTSTRAP_TOAST_AUTOHIDE']:
+                    tags.attr(data_bs_autohide='false')
                 with tags.div(Markup(msg)):
                     tags.attr(cls=f'toast-body')
+                tags.button(cls=f'{button[cat]} pt-4',
+                            data_bs_dismiss='toast')
 
     return wrap
