8ef92059e9208ed33ff2f31e5c2a3f53919cd400
Horia Filimonescu <horia.filimonescu+github@gmail.com>
2023-08-24 10:21:10 +0200

    * Bump version number to 5.3.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='Flask-BS4',
-    version='5.2.2.1.dev',
+    version='5.3.1.0',
     url='https://github.com/hfilimonescu/flask-bs4',
     license='MIT',
     author='Horia Filimonescu',
